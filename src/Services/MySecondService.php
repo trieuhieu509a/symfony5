0b04354da5396f31063e00e8bea4d32ba041df4d
hieu <htrinhtrieu@pentalog.com>
2022-01-03 23:33:50 +0700

    50. Service & autowiring by constructor injection and service arguments

---
+++ src/Services/MySecondService.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Services;
+
+Class MySecondService
+{
+    public function __construct()
+    {
+        dump('from second service');
+    }
+}
--- src/Services/MyService.php
+++ src/Services/MyService.php
@@ -3,11 +3,8 @@ namespace App\Services;
 
 Class MyService
 {
-    public function __construct($param, $param2, $adminEmail, $globalParam)
+    public function __construct(MySecondService $second_service)
     {
-        dump($param);
-        dump($param2);
-        dump($adminEmail);
-        dump($globalParam);
+        dump($second_service);
     }
 }
