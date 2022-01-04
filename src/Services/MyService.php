545bfa4120469321ca255c6294855d715b587ab4
hieu <htrinhtrieu@pentalog.com>
2022-01-04 20:02:49 +0700

    51. Service configuration

--- src/Services/MyService.php
+++ src/Services/MyService.php
@@ -3,8 +3,9 @@ namespace App\Services;
 
 Class MyService
 {
-    public function __construct(MySecondService $second_service)
+    public function __construct(MySecondService $second_service, $globalParam)
     {
         dump($second_service);
+        dump($globalParam);
     }
 }
