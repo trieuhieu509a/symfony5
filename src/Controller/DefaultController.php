a3b64a39173d48e5242736c42a5cffec4dd64783
hieu <htrinhtrieu@pentalog.com>
2022-01-08 22:54:34 +0700

    61. Cache - tags

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,7 @@ use App\Services\MyService;
 use App\Services\ServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\Cache\CacheItem;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
@@ -630,6 +631,71 @@ class DefaultController extends AbstractController
         }
 //        $cache->clear();
         dump(unserialize($posts->get()));
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => [],
+            'random_gift' => [],
+        ]);
+    }
+
+    /**
+     * @Route("/cache-tags", name="cache-tags")
+     */
+    public function cacheTags(): Response
+    {
+        $cache = new TagAwareAdapter(
+            new FilesystemAdapter()
+        );
+        /**
+         * @var CacheItem $posts
+         */
+        $acer = $cache->getItem('acer');
+        $dell = $cache->getItem('dell');
+        $ibm = $cache->getItem('ibm');
+        $apple = $cache->getItem('apple');
+
+        if (!$acer->isHit()){
+            $acer_from_db = 'acer laptop';
+            $acer->set($acer_from_db);
+            $acer->tag(['computers', 'laptops', 'acer']); // acer belong to laptops cartegory, laptops is sub-category of computers
+            $cache->save($acer);
+            dump('acer laptop form database ...');
+        }
+
+        if (!$dell->isHit()){
+            $dell_from_db = 'dell laptop';
+            $dell->set($dell_from_db);
+            $dell->tag(['computers', 'laptops', 'dell']);
+            $cache->save($dell);
+            dump('dell laptop form database ...');
+        }
+
+        if (!$ibm->isHit()){
+            $ibm_from_db = 'ibm desktops';
+            $ibm->set($ibm_from_db);
+            $ibm->tag(['computers', 'desktops', 'ibm']); // desktops NOT laptops
+            $cache->save($ibm);
+            dump('ibm laptop form database ...');
+        }
+
+        if (!$apple->isHit()){
+            $apple_from_db = 'apple desktops';
+            $apple->set($apple_from_db);
+            $apple->tag(['computers', 'desktops', 'apple']); // desktops NOT laptops
+            $cache->save($apple);
+            dump('apple laptop form database ...');
+        }
+
+//        $cache->invalidateTags(['ibm']); // invalidate tags
+//        $cache->invalidateTags(['desktops']);
+//        $cache->invalidateTags(['laptops']);
+        $cache->invalidateTags(['computers']);
+
+        dump($acer->get());
+        dump($dell->get());
+        dump($ibm->get());
+        dump($apple->get());
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
