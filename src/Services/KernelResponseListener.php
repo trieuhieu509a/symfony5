64c96e2d7407060987f37dc51531fec1efc8f140
hieu <htrinhtrieu@pentalog.com>
2022-01-10 11:35:33 +0700

    67. Create listener

---
+++ src/Services/KernelResponseListener.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class KernelResponseListener
+{
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $response = new Response('dupa');
+        $event->setResponse($response);
+    }
+}
