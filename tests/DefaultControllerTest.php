bb057866fd62ef544bab9d764b3ab0831c109aff
hieu <htrinhtrieu@pentalog.com>
2022-01-23 12:55:49 +0700

    99. Functional & unit tests - common assertions

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -12,5 +12,32 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/home');
 
         $this->assertSelectorTextContains('h1', 'Hello');
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Hello")')->count()
+        );
+        // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
+        $this->assertCount(1, $crawler->filter('h1'));
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"' // optional message shown on failure
+        );
+        $this->assertContains('foo', $client->getResponse()->getContent());
+        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
+        $this->assertTrue($client->getResponse()->isNotFound());
+        $this->assertEquals(
+            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
+            $client->getResponse()->getStatusCode()
+        );
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('/demo/contact')
+        // if the redirection URL was generated as an absolute URL
+        // $client->getResponse()->isRedirect('http://localhost/demo/contact')
+        );
+        $this->assertTrue($client->getResponse()->isRedirect());
     }
 }
