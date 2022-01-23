217ce641b244f6254356c5706dac6af4461d68c8
hieu <htrinhtrieu@pentalog.com>
2022-01-23 13:02:34 +0700

    100. Functional tests - click links

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -17,27 +17,38 @@ class DefaultControllerTest extends WebTestCase
             $crawler->filter('html:contains("Hello")')->count()
         );
         // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
-        $this->assertCount(1, $crawler->filter('h1'));
-        $this->assertTrue(
-            $client->getResponse()->headers->contains(
-                'Content-Type',
-                'application/json'
-            ),
-            'the "Content-Type" header is "application/json"' // optional message shown on failure
-        );
-        $this->assertContains('foo', $client->getResponse()->getContent());
-        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent());
-        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
-        $this->assertTrue($client->getResponse()->isNotFound());
-        $this->assertEquals(
-            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
-            $client->getResponse()->getStatusCode()
-        );
-        $this->assertTrue(
-            $client->getResponse()->isRedirect('/demo/contact')
-        // if the redirection URL was generated as an absolute URL
-        // $client->getResponse()->isRedirect('http://localhost/demo/contact')
-        );
-        $this->assertTrue($client->getResponse()->isRedirect());
+//        $this->assertCount(1, $crawler->filter('h1'));
+//        $this->assertTrue(
+//            $client->getResponse()->headers->contains(
+//                'Content-Type',
+//                'application/json'
+//            ),
+//            'the "Content-Type" header is "application/json"' // optional message shown on failure
+//        );
+//        $this->assertContains('foo', $client->getResponse()->getContent());
+//        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent());
+//        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
+//        $this->assertTrue($client->getResponse()->isNotFound());
+//        $this->assertEquals(
+//            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
+//            $client->getResponse()->getStatusCode()
+//        );
+//        $this->assertTrue(
+//            $client->getResponse()->isRedirect('/demo/contact')
+//        // if the redirection URL was generated as an absolute URL
+//        // $client->getResponse()->isRedirect('http://localhost/demo/contact')
+//        );
+//        $this->assertTrue($client->getResponse()->isRedirect());
+    }
+
+    public function testClickLink(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/home');
+
+        $link = $crawler->filter('a:contains("Login")')->link();
+        $crawler = $client->click($link);
+
+        $this->assertStringContainsString('Remember me', $client->getResponse()->getContent());
     }
 }
