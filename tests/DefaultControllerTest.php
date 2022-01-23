da4e4a33b992695cb31c119c59806b052202926e
hieu <htrinhtrieu@pentalog.com>
2022-01-23 22:56:05 +0700

    103. Functional tests - tests in isolation

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -2,44 +2,66 @@
 
 namespace App\Tests;
 
+use App\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/home');
+    private $entityManager;
 
-        $this->assertSelectorTextContains('h1', 'Hello');
-        $this->assertGreaterThan(
-            0,
-            $crawler->filter('html:contains("Hello")')->count()
-        );
-        // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
-//        $this->assertCount(1, $crawler->filter('h1'));
-//        $this->assertTrue(
-//            $client->getResponse()->headers->contains(
-//                'Content-Type',
-//                'application/json'
-//            ),
-//            'the "Content-Type" header is "application/json"' // optional message shown on failure
-//        );
-//        $this->assertContains('foo', $client->getResponse()->getContent());
-//        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent());
-//        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
-//        $this->assertTrue($client->getResponse()->isNotFound());
-//        $this->assertEquals(
-//            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
-//            $client->getResponse()->getStatusCode()
-//        );
-//        $this->assertTrue(
-//            $client->getResponse()->isRedirect('/demo/contact')
-//        // if the redirection URL was generated as an absolute URL
-//        // $client->getResponse()->isRedirect('http://localhost/demo/contact')
-//        );
-//        $this->assertTrue($client->getResponse()->isRedirect());
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+
+        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $this->entityManager->beginTransaction();
+        $this->entityManager->getConnection()->setAutoCommit(false);
     }
+
+    protected function tearDown(): void
+    {
+        $this->entityManager->rollback();
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+
+//    public function testSomething(): void
+//    {
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', '/home');
+//
+//        $this->assertSelectorTextContains('h1', 'Hello');
+//        $this->assertGreaterThan(
+//            0,
+//            $crawler->filter('html:contains("Hello")')->count()
+//        );
+//        // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
+////        $this->assertCount(1, $crawler->filter('h1'));
+////        $this->assertTrue(
+////            $client->getResponse()->headers->contains(
+////                'Content-Type',
+////                'application/json'
+////            ),
+////            'the "Content-Type" header is "application/json"' // optional message shown on failure
+////        );
+////        $this->assertContains('foo', $client->getResponse()->getContent());
+////        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent());
+////        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
+////        $this->assertTrue($client->getResponse()->isNotFound());
+////        $this->assertEquals(
+////            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
+////            $client->getResponse()->getStatusCode()
+////        );
+////        $this->assertTrue(
+////            $client->getResponse()->isRedirect('/demo/contact')
+////        // if the redirection URL was generated as an absolute URL
+////        // $client->getResponse()->isRedirect('http://localhost/demo/contact')
+////        );
+////        $this->assertTrue($client->getResponse()->isRedirect());
+//    }
 //
 //    public function testClickLink(): void
 //    {
@@ -66,22 +88,40 @@ class DefaultControllerTest extends WebTestCase
 //
 //        $this->assertEquals(1, $crawler->filter('a:contains("logout")')->count());
 //    }
+//
+//    /**
+//     * @dataProvider provideUrls
+//     */
+//    public function testDataProvider($url): void
+//    {
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', $url);
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+//    }
+//
+//    public function provideUrls()
+//    {
+//        return [
+//            ['/home'],
+//            ['/login']
+//        ];
+//    }
 
-    /**
-     * @dataProvider provideUrls
-     */
-    public function testDataProvider($url): void
+    public function testInIsolation()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', $url);
-        $this->assertTrue($client->getResponse()->isSuccessful());
-    }
+        $crawler = $this->client->request('GET', '/home');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
 
-    public function provideUrls()
-    {
-        return [
-            ['/home'],
-            ['/login']
-        ];
+        $video = $this->entityManager
+            ->getRepository(Video::class)
+            ->find(7);
+
+        $video = $this->entityManager->merge($video);
+        $this->entityManager->remove($video);
+        $this->entityManager->flush();
+
+        $this->assertNull($this->entityManager
+            ->getRepository(Video::class)
+            ->find(7));
     }
 }
